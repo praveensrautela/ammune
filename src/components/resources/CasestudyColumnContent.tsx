a18069692983d5b31387b2944e167e9ab59ba1f9
praveen <55490713+praveensrautela@users.noreply.github.com>
2025-09-25 16:41:11 +0530

    Update CasestudyColumnContent.tsx
    update name

--- src/components/resources/CasestudyColumnContent.tsx
+++ src/components/resources/CasestudyColumnContent.tsx
@@ -1,4 +1,4 @@
-import ServiceListData from "../../assets/jsonData/resources/CaseStudyData.json";
+import ServiceListData from "../../assets/jsonData/Resources/CaseStudyData.json";
 
 import CaseStudylist from "../services/Casestudylist";
 import { Link } from "react-router-dom";
