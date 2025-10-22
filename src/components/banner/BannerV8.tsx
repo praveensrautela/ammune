4054f105407fb3ec5fd4e60f58405adbafbfc56a
praveen <rautelapraveen20@gmail.com>
2025-10-22 18:52:31 +0530

    update delay

--- src/components/banner/BannerV8.tsx
+++ src/components/banner/BannerV8.tsx
@@ -21,7 +21,7 @@ const BannerV8 = () => {
                                     <h2>
                                         <SplitText
                                             className="title-left split-text"
-                                            delay={150}
+                                            delay={40}
                                             animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
                                             animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
                                             easing="easeOutCubic"
