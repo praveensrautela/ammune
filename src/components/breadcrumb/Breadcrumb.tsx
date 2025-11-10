8b0bb3f090f357f88e20cac4e23b5503ae1cf674
praveen <rautelapraveen20@gmail.com>
2025-11-10 11:37:14 +0530

    update contact form

--- src/components/breadcrumb/Breadcrumb.tsx
+++ src/components/breadcrumb/Breadcrumb.tsx
@@ -13,7 +13,7 @@ const Breadcrumb = ({ title, breadCrumb }: DataType) => {
                 <div className="light-banner-active bg-gray bg-cover" style={{ backgroundImage: 'url(/assets/img/shape/6.jpg)' }} />
 
                 <div className="container blurry-shape-right blurry-shape-top-left">
-                    <div className="breadcrumb-item">
+                    <div className="breadcrumb-item" style={{padding: '170px 0px 64px 0px'}}>
                         <div className="row">
                             <div className="col-lg-8 offset-lg-2">
                                 <h1>{title ? title : "not found"}</h1>
--- src/components/contact/ContactV1.tsx
+++ src/components/contact/ContactV1.tsx
@@ -16,20 +16,20 @@ const ContactV1 = ({ sectionClass }: DataType) => {
                                 <div className="contact-style-one-info">
                                     <ul className="contact-address">
                                         <li>
-                                            <a className="phone-link" href="tel:+4733378901"><i className="fas fa-user-headset" /> +4733378901</a>
+                                            <a className="phone-link" href="tel:+1(201) 885-4585"><i className="fas fa-user-headset" /> +1(201) 885-4585</a>
                                         </li>
                                         <li>
                                             <div className="info">
                                                 <h4>Location</h4>
                                                 <p>
-                                                    55 Main Street, The Grand Avenue <br /> 2nd Block, New York City
+                                                    324 east 9th street Brooklyn NY 11218
                                                 </p>
                                             </div>
                                         </li>
                                         <li>
                                             <div className="info">
                                                 <h4>Official Email</h4>
-                                                <a href="mailto:info@digital.com.com">info@digital.com</a>
+                                                <a href="mailto:info@ammune.ai">info@ammune.ai</a>
                                             </div>
                                         </li>
                                         <li>
